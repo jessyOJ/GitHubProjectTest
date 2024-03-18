d6d53302287e37883889e04ece7a419a51c8f2bc
jessyOJ <jessicachioma23@outlook.com>
2024-03-18 14:45:54 +0100

    first commit

---
+++ src/Githubtest/hungryTest.java
@@ -0,0 +1,14 @@
+package Githubtest;
+
+public class hungryTest {
+
+	public static void main(String[] args) {
+		String hungry ="no";
+		if(hungry=="yes") 
+			System.out.println("go eat");
+		else
+			System.out.println("go do some work");
+
+	}
+
+}
